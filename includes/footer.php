b3bceea12134d633dc46f431e711a2ea6b0d5214
LouiseEmike <emykay.el@gmail.com>
2019-04-01 12:58:22 -0700

    first push

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+    </div>
+<div id="footer">Copyright 2007, Widget Corp</div>
+<?php
+    //5 Close connection
+    if(isset($connection)){
+        mysql_close($connection);
+    }
+?>
+<script>
+   
+</script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Widget Corp</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
+    <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+    <style>
+        .red { color: #8D0D19; }
+    </style>
+    <!-- <script src="main.js"></script> -->
+</head>
+<body>
+    <div id="header">
+        <h1>Widget Corp</h1>
+    </div>
+    <div id="main">
